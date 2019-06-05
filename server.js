0907b07259ec15e635320f4a91f852dad2f782f8
Nicholas Sinai <nicksinai3@gmail.com>
2019-06-04 22:19:30 -0400

    Start data model

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/users', require('./routes/api/users'))
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
