0b7b95788dcf2f6a78306859229e0954009a3bd0
Nicholas Sinai <nicksinai3@gmail.com>
2019-06-05 16:49:36 -0400

    Create User Model

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // @route   GET api/users
-// @desc    Test route
+// @desc    Sign Up User
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('User route')
+});
 
 module.exports = router;
