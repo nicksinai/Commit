eed11257f9113054a9880b5ffb6a9028592234a2
Nicholas Sinai <nicksinai3@gmail.com>
2019-06-05 16:49:36 -0400

    Create User Model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,28 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    
+    email: {
+        type: String,
+        requird: true,
+        unique: true
+    },
+    active: {
+        type: boolean,
+        default: false
+    },
+    gym: {
+        type: String
+    },
+    paymentInfo: {
+        type: String
+    },
+    lastCheckin: {
+        type: Date
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
 });
+
+module.exports = User = mongoose.model('user', UserSchema);
