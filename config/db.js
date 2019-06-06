f30666d7f9406cdadc1b0da6596dd5cb6edb7012
Nicholas Sinai <nicksinai3@gmail.com>
2019-06-06 15:17:22 -0400

    Sign up user no token

--- config/db.js
+++ config/db.js
@@ -4,12 +4,15 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, { useNewUrlParser: true });
-        console.log("MongoDB Connected...");
-    } catch(err) {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true
+        });
+        console.log('MongoDB Connected...');
+    } catch (err) {
         console.error(err.message);
         process.exit(1);
     }
-}
+};
 
 module.exports = connectDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,29 +1,65 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
+const User = require('../../models/User');
 
 // @route   GET api/users
 // @desc    Sign Up User
 // @access  Public
 router.post(
-  '/',
-  [
-    check('name', 'Name is required')
-      .not()
-      .isEmpty(),
-    check('email', 'Please include avalid email').isEmail(),
-    check(
-      'password',
-      'Please enter a password with 8 or more characters'
-    ).isLength({ min: 8 })
-  ],
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    '/',
+    [
+        check('name', 'Name is required')
+            .not()
+            .isEmpty(),
+        check('email', 'Please include avalid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 8 or more characters'
+        ).isLength({ min: 8 })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, email, password } = req.body;
+
+        try {
+            // Check if user exists
+            let user = await User.findOne({ email });
+
+            if (user) {
+                return res.status(400).json({
+                    errors: [{ msg: 'User already exists' }]
+                });
+            }
+
+            user = new User({
+                name,
+                email,
+                password
+            });
+
+            // Encrypt password
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(user.password, salt);
+
+            await user.save();
+
+            res.send('User signed up');
+
+            // Return JWT
+
+            res.send('User route');
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
     }
-    res.send('User route');
-  }
 );
 
 module.exports = router;
