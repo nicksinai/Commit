1b3a0bfbd9f924d59b89b2edc90b280d818fb881
Nicholas Sinai <nicksinai3@gmail.com>
2019-06-21 17:59:15 -0400

    Create React App

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const connectDB = require('./config/db');
+const db = require('./config/db');
 
 const app = express();
 
 // Connect Database
-connectDB();
+db.connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
